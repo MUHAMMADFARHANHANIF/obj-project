6b89125d259d0b65245e046ad8a7b4b3758ac94b
MUHAMMADFARHANHANIF <farhansmit0318@gamil.com>
2024-05-03 04:14:34 -0700

    half work

--- app.js
+++ app.js
@@ -1,142 +1,211 @@
-let users = document.querySelector('#users')
+// // let users = document.querySelector('#users')
+
+// // const username = document.querySelector('#inputname')
+// // const email = document.querySelector('#inputemail')
+// // const age = document.querySelector('#inputage')
+// // const city = document.querySelector('#inputcity')
 
 
 
+// // let usersarray = [
+     
+// // ];
 
 
-
-function submint() {
+// // function submint() {
     
-const username = document.querySelector('#inputname')
-const email = document.querySelector('#inputemail')
-const age = document.querySelector('#inputage')
-const city = document.querySelector('#inputcity')
+    
+// //     // usersarray = [
+     
+// //     // ];
 
+
+    
+// //     let obj = {
+// //         username: username.value,
+// //         email: email.value,
+// //         age: age.value,
+// //         city: city.value,
+// //     }
+    
+
+
+// // usersarray.push(obj)
+ 
+
+
+
+// // for(let i = 0 ; i < usersarray.length ; i++){
+// // //   users.innerHTML = ""
+
+// // //    users.innerHTML +=  `
+// // //    <div class="card" style="width: 18rem;">
+   
+
+// // //    <img src="https://img.freepik.com/free-vector/creative-welcome-team-banner-corporate-hiring_1017-43282.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714608000&semt=sph" class="card-img-top" alt="...">
+// // //    <div class="card-body">
+// // //      <h5 class="card-title">${usersarray[i]}</h5>
+
+// // //      <p class="card-text">Hamare team mein aane ke liye khushamdeed! Aapka tajurba aur josh hamare liye ahem hain 
+// // //      </p>
+// // //    </div>
+// // //    <ul class="list-group list-group-flush ">
+// // //      <li class="list-group-item bg-warning "><h3>Name [ ${usersarray[i].username}</h3></li>
+// // //      <li class="list-group-item">Age : ${usersarray[i].age} </h3></li>
+// // //      <li class="list-group-item">City : ${usersarray[i].city}</h3> </h2></li>
+
+// // //      <li class="list-group-item">Email : ${usersarray[i].email}</h3></li>
+// // //    </ul>
+  
+// // //  </div> `
+
+// // console.log(usersarray);
+
+// //  console.log(usersarray.length);
+     
+
+
+
+
+
+// // }
+
+// // username.value =''
+// // email.value = ''
+// // age.value = ''
+// // city.value = ''
+
+// // // usersarray = []
+// // }
+
+
+// let users = document.querySelector('#users');
+
+// const username = document.querySelector('#inputname');
+// const email = document.querySelector('#inputemail');
+// const age = document.querySelector('#inputage');
+// const city = document.querySelector('#inputcity');
+
+// let usersarray = [];
+
+// function submit() {
+//     let obj = {
+//         username: username.value,
+//         email: email.value,
+//         age: age.value,
+//         city: city.value,
+//     };
+    
+//     usersarray.push(obj);
+
+//     // Clear the input fields after submission
+//     username.value = '';
+//     email.value = '';
+//     age.value = '';
+//     city.value = '';
+
+
+//     // Display the users as cards
+//     displayUsers();
+// }
+
+// function displayUsers(index) {
+//     users.innerHTML = ''; // Clear previous content
+//     usersarray.forEach(user => {
+//         users.innerHTML += `
+//             <div class="card" style="width: 18rem;">
+//                 <img src="https://img.freepik.com/free-vector/creative-welcome-team-banner-corporate-hiring_1017-43282.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714608000&semt=sph" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${user.username}</h5>
+//                     <p class="card-text">Hamare team mein aane ke liye khushamdeed! Aapka tajurba aur josh hamare liye ahem hain</p>
+//                 </div>
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item bg-warning"><h3>Name: ${user.username}</h3></li>
+//                     <li class="list-group-item">Age: ${user.age}</li>
+//                     <li class="list-group-item">City: ${user.city}</li>
+//                     <li class="list-group-item">Email: ${user.email}</li>
+//                 </ul>
+//             </div>
+//         `;
+//     });
+
+//     // console.log(index);
+// }
+
+
+
+
+
+
+
+
+
+
+let users = document.querySelector('#users');
+
+const username = document.querySelector('#inputname');
+const email = document.querySelector('#inputemail');
+const age = document.querySelector('#inputage');
+const city = document.querySelector('#inputcity');
+
+let usersarray = [];
+
+function submit() {
     let obj = {
         username: username.value,
         email: email.value,
         age: age.value,
         city: city.value,
+    };
+    
+    usersarray.push(obj);
+
+    // Clear the input fields after submission
+    username.value = '';
+    email.value = '';
+    age.value = '';
+    city.value = '';
+
+    // Display the users as cards
+    displayUsers();
+}
+
+function displayUsers() {
+    users.innerHTML = ''; // Clear previous content
+    for (let i = 0; i < usersarray.length; i++) {
+        let user = usersarray[i];
+        users.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img src="https://img.freepik.com/free-vector/creative-welcome-team-banner-corporate-hiring_1017-43282.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714608000&semt=sph" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${i}</h5>
+                    <p class="card-text">Hamare team mein aane ke liye khushamdeed! Aapka tajurba aur josh hamare liye ahem hain</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item bg-warning"><h3>Name: ${user.username}</h3></li>
+                    <li class="list-group-item">Age: ${user.age}</li>
+                    <li class="list-group-item">City: ${user.city}</li>
+                    <li class="list-group-item">Email: ${user.email}</li>
+                </ul>
+                <button type="submit" onclick="delet(${i})">submint</button>
+            </div>
+
+            
+        `;
     }
-
-
-
-
-
-let usersarray = [
-    // {
-    //     name: "Alice",
-    //     age: 25,
-    //     hobbies: ["Reading", "Painting"]
-    // },
-    // {
-    //     name: "Bob",
-    //     age: 30,
-    //     hobbies: ["Playing guitar", "Cooking"]
-    // },
-    // {
-    //     name: "Charlie",
-    //     age: 22,
-    //     hobbies: ["Hiking", "Photography"]
-    // },
-    // {
-    //     name: "David",
-    //     age: 28,
-    //     hobbies: ["Running", "Watching movies"]
-    // },
-    // {
-    //     name: "Ella",
-    //     age: 35,
-    //     hobbies: ["Traveling", "Knitting"]
-    // },
-    // {
-    //     name: "Frank",
-    //     age: 20,
-    //     hobbies: ["Gaming", "Drawing"]
-    // },
-    // {
-    //     name: "Grace",
-    //     age: 27,
-    //     hobbies: ["Singing", "Dancing"]
-    // },
-    // {
-    //     name: "Hannah",
-    //     age: 32,
-    //     hobbies: ["Cooking", "Yoga"]
-    // },
-    // {
-    //     name: "Ian",
-    //     age: 24,
-    //     hobbies: ["Playing football", "Reading"]
-    // },
-    // {
-    //     name: "Jessica",
-    //     age: 29,
-    //     hobbies: ["Writing", "Cycling" , 'swimming']
-    // }
-];
-
-usersarray.push(obj)
- 
-// for(let j = 999 ; j  < 100   00 ; j++){
-
-
-for(let i = 0 ; i  < usersarray.length ; i++){
-    // let hobbieslist = ''
-
-    // let cardNo = use
-    // for (let j = 0; j < usersarray[i].hobbies.length; j++) {
-    //   hobbieslist.innerHTML +=  `<li>${usersarray[i].hobbies[j]}</li>`
-        
-    // }
-    // for(let j = 0 ; i  < usersarray.length ; i++){
-
-
-    // console.log(hobbieslist);
-   users.innerHTML +=  `
-   <div class="card" style="width: 18rem;">
-   <img src="https://img.freepik.com/free-vector/creative-welcome-team-banner-corporate-hiring_1017-43282.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714608000&semt=sph" class="card-img-top" alt="...">
-   <div class="card-body">
-     <h5 class="card-title">${usersarray[i].username}</h5>
-
-     <p class="card-text">Hamare team mein aane ke liye khushamdeed! Aapka tajurba aur josh hamare liye ahem hain 
-     </p>
-   </div>
-   <ul class="list-group list-group-flush ">
-     <li class="list-group-item bg-warning "><h3>Name [ ${usersarray[i].username}</h3></li>
-     <li class="list-group-item">Age : ${usersarray[i].age} </h3></li>
-     <li class="list-group-item">City : ${usersarray[i].city}</h3> </h2></li>
-
-     <li class="list-group-item">Email : ${usersarray[i].email}</h3></li>
-   </ul>
-  
- </div>
-
-
-
-
-
-
-
-    
-
-    
-
-
-    
-    
-     `
- console.log(usersarray.length);
-     
-
-
-
-}
- username.value =''
-     email.value = ''
-     age.value = ''
- city.value = ''
-
-
 }
 
+
+function delet(index) {
+console.log(usersarray[index].username);
+console.log(usersarray[index].age);
+
+// console.log(index.age);
+
+
+    // const updatedVal = prompt('enter updated value');
+    // usersarray.splice(index , 1 , updatedVal);
+
+
+    // submit()
+}
